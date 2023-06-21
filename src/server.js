00288dec4d226dc8629386ef75df335052bf2eb4
Devon Fazekas <fazekas.devon@gmail.com>
2023-06-21 04:04:45 -0400

    feat: Add CORS

--- src/server.js
+++ src/server.js
@@ -2,12 +2,15 @@ require('./config/db')
 
 const routes = require('./routes')
 const bodyParser = require('body-parser')
+const cors = require('cors')
 const app = require('express')()
 
 // Parse URL-encoded bodies
 app.use(bodyParser.urlencoded({ extended: false }))
 // Parse JSON bodies
 app.use(bodyParser.json())
+// Enable CORS
+app.use(cors({ origin: true, credentials: true }))
 
 app.get('/', (req, res) => res.send('Hello World!'))
 app.use(routes)
