9f6198a8af921b866c10f08fc72602d232a76b42
Devon Fazekas <fazekas.devon@gmail.com>
2023-07-07 00:34:14 -0400

    refactor: Simplify `addPlace` method

--- src/domains/places/controller.js
+++ src/domains/places/controller.js
@@ -8,35 +8,9 @@ const placeTypes = require('./placeTypes.json')
 
 const addPlace = async (req, res, next) => {
   try {
-    console.log(req.body)
-
-    const originalObject = req.body
-
-    const convertedObject = {
-      type: 'Point',
-      coordinates: [
-        originalObject.geometry.location.lng,
-        originalObject.geometry.location.lat
-      ],
-      properties: {
-        business_status: originalObject.business_status,
-        icon: originalObject.icon,
-        name: originalObject.name,
-        opening_hours:
-          originalObject.opening_hours && originalObject.opening_hours.open_now
-            ? 'Open'
-            : 'Closed',
-        photos: originalObject.photos,
-        place_id: originalObject.place_id,
-        rating: originalObject.rating,
-        reference: originalObject.reference,
-        scope: originalObject.scope,
-        types: originalObject.types,
-        user_ratings_total: originalObject.user_ratings_total,
-        vicinity: originalObject.vicinity
-      }
-    }
-    const place = await PlaceModel.create(convertedObject)
+    const city = req.params.city
+    const data = req.body
+    const place = await PlaceModel.new(data, city).save()
     res.status(201).json(place)
   } catch (error) {
     next(error)
