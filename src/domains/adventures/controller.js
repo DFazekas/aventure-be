3b3af7197799a62392af9e15d7b12c22b9018d3f
Devon Fazekas <fazekas.devon@gmail.com>
2023-09-16 01:23:32 -0400

    feat: Update Adventure controller's getAdventures method
    - Modified the getAdventures method to align with the new GET /adventures endpoint requirements.
    - Introduced pagination logic.
    - Replaced existing query parameters with `state`, `page`, and `limit`.
    - Included '_id' field as part of the adventure response object.
    - Enhanced error handling.
    - Changed the hardcoded limit value from 10 to parameter `limit`.
    - Changed the nature of the API to always return a value (status 200) instead of 404 when no adventures are found.

--- src/domains/adventures/controller.js
+++ src/domains/adventures/controller.js
@@ -42,25 +42,59 @@ const createAdventure = async (req, res, next) => {
 }
 
 const getAdventures = async (req, res, next) => {
-  let queryOptions = {}
   try {
-    const categories = req.query.categories
-    if (categories.length > 0) {
-      queryOptions = {
-        'metadata.tags': { $all: categories.split(',') }
-      }
+    // Extract the query parameters.
+    const { state, page = 1, limit = 20 } = req.query
+
+    // Validate the `state` parameter.
+    if (state !== 'active' && state !== 'inactive') {
+      return res.status(400).json({
+        error: 'Invalid state parameter. Must be "active" or "inactive".'
+      })
     }
 
-    console.log(`categories:`, categories.split(','))
-    const adventures = await AdventureModel.find(queryOptions, '-_id -__v')
-      .limit(10)
+    // Ensure the `limit` parameter is capped at a maximum value of 50.
+    const minLimit = Math.max(Number(limit), 1)
+    const cappedLimit = Math.min(Number(minLimit), 50)
+
+    // Query the database to get the total count of adventures.
+    const totalAdventures = await AdventureModel.countDocuments({ state })
+
+    // Calculate the maximum valid page based on the total number of adventures.
+    const maxPage = Math.max(Math.ceil(totalAdventures / cappedLimit), 1)
+
+    // Ensure the requested `page` is within valid range.
+    const validPage = Math.min(Number(page), maxPage)
+
+    // Calculate the skip value for pagination.
+    const skip = (validPage - 1) * cappedLimit
+
+    // Check if the calculated skip value is negative (page out of range).
+    if (skip < 0) {
+      const error = new Error('Page number must be positive.')
+      error.status = 400
+      throw error
+    }
+
+    // Query the database based on the state and pagination parameters.
+    const adventures = await AdventureModel.find(
+      { state },
+      '-itinerary -metadata -__v'
+    )
+      .skip(skip)
+      .limit(cappedLimit)
       .exec()
 
-    if (adventures.length === 0) {
-      res.status(404).json({ message: 'No adventures found' })
-      return
+    // Prepare the response as JSON.
+    const response = {
+      adventures,
+      pagination: {
+        page: validPage,
+        limit: cappedLimit,
+        total: totalAdventures
+      }
     }
-    res.status(200).json(adventures)
+    res.status(200).json(response)
   } catch (error) {
     next(error)
   }
